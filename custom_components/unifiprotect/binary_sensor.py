7439c9c74f4191e22d0dc0492b358e03d7fe480b
JoeStratton <joe.stratton@asu.edu>
2020-11-20 00:16:13 -0700

    updating to new location and version, cleaned up old unused code and files

--- custom_components/unifiprotect/binary_sensor.py
+++ custom_components/unifiprotect/binary_sensor.py
@@ -1,9 +1,5 @@
-""" This component provides binary sensors for Unifi Protect."""
+"""This component provides binary sensors for Unifi Protect."""
 import logging
-import voluptuous as vol
-from datetime import timedelta
-
-import homeassistant.helpers.config_validation as cv
 
 try:
     from homeassistant.components.binary_sensor import (
@@ -12,72 +8,91 @@ try:
 except ImportError:
     # Prior to HA v0.110
     from homeassistant.components.binary_sensor import BinarySensorDevice
-from homeassistant.components.binary_sensor import DEVICE_CLASS_MOTION
-from homeassistant.const import (
-    ATTR_ATTRIBUTION,
-    ATTR_FRIENDLY_NAME,
-    ATTR_LAST_TRIP_TIME,
+
+from homeassistant.components.binary_sensor import (
+    DEVICE_CLASS_MOTION,
+    DEVICE_CLASS_OCCUPANCY,
 )
-from homeassistant.helpers.config_validation import PLATFORM_SCHEMA
-from . import (
-    UPV_DATA,
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import ATTR_ATTRIBUTION, ATTR_LAST_TRIP_TIME
+from homeassistant.helpers.typing import HomeAssistantType
+
+
+from .const import (
+    ATTR_EVENT_LENGTH,
+    ATTR_EVENT_OBJECT,
+    ATTR_EVENT_SCORE,
     DEFAULT_ATTRIBUTION,
-    DEFAULT_BRAND,
-    DEVICE_CLASS_DOORBELL,
+    DEVICE_TYPE_MOTION,
+    DEVICE_TYPE_DOORBELL,
+    DOMAIN,
 )
+from .entity import UnifiProtectEntity
 
 _LOGGER = logging.getLogger(__name__)
 
-DEPENDENCIES = ["unifiprotect"]
+PROTECT_TO_HASS_DEVICE_CLASS = {
+    DEVICE_TYPE_DOORBELL: DEVICE_CLASS_OCCUPANCY,
+    DEVICE_TYPE_MOTION: DEVICE_CLASS_MOTION,
+}
 
-ATTR_BRAND = "brand"
-ATTR_EVENT_SCORE = "event_score"
 
-
-async def async_setup_platform(hass, config, async_add_entities, _discovery_info=None):
-    """Set up an Unifi Protect binary sensor."""
-    coordinator = hass.data[UPV_DATA]["coordinator"]
-    if not coordinator.data:
+async def async_setup_entry(
+    hass: HomeAssistantType, entry: ConfigEntry, async_add_entities
+) -> None:
+    """Set up binary sensors for UniFi Protect integration."""
+    entry_data = hass.data[DOMAIN][entry.entry_id]
+    upv_object = entry_data["upv"]
+    protect_data = entry_data["protect_data"]
+    if not protect_data.data:
         return
 
     sensors = []
-    for camera in coordinator.data:
-        if coordinator.data[camera]["type"] == "doorbell":
-            sensors.append(UfpBinarySensor(coordinator, camera, DEVICE_CLASS_DOORBELL))
-        sensors.append(UfpBinarySensor(coordinator, camera, DEVICE_CLASS_MOTION))
+    for camera_id in protect_data.data:
+        camera_data = protect_data.data[camera_id]
+        if camera_data["type"] == DEVICE_TYPE_DOORBELL:
+            sensors.append(
+                UnifiProtectBinarySensor(
+                    upv_object, protect_data, camera_id, DEVICE_TYPE_DOORBELL
+                )
+            )
+            _LOGGER.debug(
+                "UNIFIPROTECT DOORBELL SENSOR CREATED: %s",
+                camera_data["name"],
+            )
 
-    async_add_entities(sensors, True)
+        sensors.append(
+            UnifiProtectBinarySensor(
+                upv_object, protect_data, camera_id, DEVICE_TYPE_MOTION
+            )
+        )
+        _LOGGER.debug("UNIFIPROTECT MOTION SENSOR CREATED: %s", camera_data["name"])
+
+    async_add_entities(sensors)
+
+    return True
 
 
-class UfpBinarySensor(BinarySensorDevice):
+class UnifiProtectBinarySensor(UnifiProtectEntity, BinarySensorDevice):
     """A Unifi Protect Binary Sensor."""
 
-    def __init__(self, coordinator, camera, sensor_type):
-        self.coordinator = coordinator
-        self._camera_id = camera
-        self._camera = coordinator.data[camera]
-        self._name = f"{sensor_type.capitalize()} {self._camera['name']}"
-        self._unique_id = self._name.lower().replace(" ", "_")
-        self._device_class = sensor_type
-        self._event_score = self._camera["event_score"]
-
-        if self._device_class == DEVICE_CLASS_DOORBELL:
-            _LOGGER.debug(f"UNIFIPROTECT DOORBELL SENSOR CREATED: {self._name}")
-        else:
-            _LOGGER.debug(f"UNIFIPROTECT MOTION SENSOR CREATED: {self._name}")
+    def __init__(self, upv_object, protect_data, camera_id, sensor_type):
+        """Initialize the Binary Sensor."""
+        super().__init__(upv_object, protect_data, camera_id, sensor_type)
+        self._name = f"{sensor_type.capitalize()} {self._camera_data['name']}"
+        self._device_class = PROTECT_TO_HASS_DEVICE_CLASS.get(sensor_type)
 
     @property
-    def unique_id(self):
-        """Return a unique ID."""
-        return self._unique_id
+    def name(self):
+        """Return name of the sensor."""
+        return self._name
 
     @property
     def is_on(self):
         """Return true if the binary sensor is on."""
-        if self._device_class == DEVICE_CLASS_DOORBELL:
-            return self.coordinator.data[self._camera_id]["event_ring_on"]
-        else:
-            return self.coordinator.data[self._camera_id]["event_on"]
+        if self._sensor_type == DEVICE_TYPE_DOORBELL:
+            return self._camera_data["event_ring_on"]
+        return self._camera_data["event_on"]
 
     @property
     def device_class(self):
@@ -87,45 +102,28 @@ class UfpBinarySensor(BinarySensorDevice):
     @property
     def icon(self):
         """Select icon to display in Frontend."""
-        if self._device_class == DEVICE_CLASS_DOORBELL:
-            if self.coordinator.data[self._camera_id]["event_ring_on"]:
+        if self._sensor_type == DEVICE_TYPE_DOORBELL:
+            if self._camera_data["event_ring_on"]:
                 return "mdi:bell-ring-outline"
-            else:
-                return "mdi:doorbell-video"
+            return "mdi:doorbell-video"
 
     @property
     def device_state_attributes(self):
         """Return the device state attributes."""
-        attrs = {}
-
-        attrs[ATTR_ATTRIBUTION] = DEFAULT_ATTRIBUTION
-        attrs[ATTR_BRAND] = DEFAULT_BRAND
-        attrs[ATTR_FRIENDLY_NAME] = self._name
-        if self._device_class == DEVICE_CLASS_DOORBELL:
-            attrs[ATTR_LAST_TRIP_TIME] = self.coordinator.data[self._camera_id][
-                "last_ring"
-            ]
+        if self._sensor_type == DEVICE_TYPE_DOORBELL:
+            return {
+                ATTR_ATTRIBUTION: DEFAULT_ATTRIBUTION,
+                ATTR_LAST_TRIP_TIME: self._camera_data["last_ring"],
+            }
+        if len(self._camera_data["event_object"]) > 0:
+            detected_object = self._camera_data["event_object"][0]
+            _LOGGER.debug(f"OBJECTS: {self._camera_data['event_object']}")
         else:
-            attrs[ATTR_LAST_TRIP_TIME] = self.coordinator.data[self._camera_id][
-                "last_motion"
-            ]
-            attrs[ATTR_EVENT_SCORE] = self.coordinator.data[self._camera_id][
-                "event_score"
-            ]
-        return attrs
-
-    @property
-    def should_poll(self):
-        """No need to poll. Coordinator notifies entity of updates."""
-        return False
-
-    @property
-    def available(self):
-        """Return if entity is available."""
-        return self.coordinator.last_update_success
-
-    async def async_added_to_hass(self):
-        """When entity is added to hass."""
-        self.async_on_remove(
-            self.coordinator.async_add_listener(self.async_write_ha_state)
-        )
+            detected_object = "None Identified"
+        return {
+            ATTR_ATTRIBUTION: DEFAULT_ATTRIBUTION,
+            ATTR_LAST_TRIP_TIME: self._camera_data["last_motion"],
+            ATTR_EVENT_SCORE: self._camera_data["event_score"],
+            ATTR_EVENT_LENGTH: self._camera_data["event_length"],
+            ATTR_EVENT_OBJECT: detected_object,
+        }
--- custom_components/unifiprotect/switch.py
+++ custom_components/unifiprotect/switch.py
@@ -1,10 +1,6 @@
-""" This component provides Switches for Unifi Protect."""
+"""This component provides Switches for Unifi Protect."""
 
 import logging
-import voluptuous as vol
-from datetime import timedelta
-
-import homeassistant.helpers.config_validation as cv
 
 try:
     from homeassistant.components.switch import SwitchEntity as SwitchDevice
@@ -12,117 +8,96 @@ except ImportError:
     # Prior to HA v0.110
     from homeassistant.components.switch import SwitchDevice
 
-from homeassistant.components.switch import PLATFORM_SCHEMA
-from homeassistant.const import (
-    ATTR_ATTRIBUTION,
-    ATTR_FRIENDLY_NAME,
-    CONF_MONITORED_CONDITIONS,
-)
-from homeassistant.helpers.config_validation import PLATFORM_SCHEMA
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import ATTR_ATTRIBUTION
+from homeassistant.helpers.typing import HomeAssistantType
 
-from . import (
-    UPV_DATA,
+from .const import (
+    ATTR_CAMERA_TYPE,
+    CONF_IR_OFF,
+    CONF_IR_ON,
     DEFAULT_ATTRIBUTION,
-    DEFAULT_BRAND,
     DOMAIN,
+    TYPE_HIGH_FPS_OFF,
+    TYPE_HIGH_FPS_ON,
     TYPE_RECORD_ALLWAYS,
     TYPE_RECORD_MOTION,
     TYPE_RECORD_NEVER,
-    TYPE_IR_AUTO,
-    TYPE_IR_OFF,
-    TYPE_IR_LED_OFF,
-    TYPE_IR_ON,
 )
+from .entity import UnifiProtectEntity
 
 _LOGGER = logging.getLogger(__name__)
 
-DEPENDENCIES = ["unifiprotect"]
-
-SCAN_INTERVAL = timedelta(seconds=5)
-
-ATTR_CAMERA_TYPE = "camera_type"
-ATTR_BRAND = "brand"
-
-CONF_IR_ON = "ir_on"
-CONF_IR_OFF = "ir_off"
-
-DEFAULT_IR_ON = TYPE_IR_AUTO
-DEFAULT_IR_OFF = TYPE_IR_OFF
-
 SWITCH_TYPES = {
-    "record_motion": ["Record Motion", "camcorder", "record_motion"],
-    "record_always": ["Record Always", "camcorder", "record_always"],
+    "record_motion": ["Record Motion", "video-outline", "record_motion"],
+    "record_always": ["Record Always", "video", "record_always"],
     "ir_mode": ["IR Active", "brightness-4", "ir_mode"],
+    "status_light": ["Status Light On", "led-on", "status_light"],
+    "hdr_mode": ["HDR Mode", "brightness-7", "hdr_mode"],
+    "high_fps": ["High FPS", "video-high-definition", "high_fps"],
 }
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {
-        vol.Required(CONF_MONITORED_CONDITIONS, default=list(SWITCH_TYPES)): vol.All(
-            cv.ensure_list, [vol.In(SWITCH_TYPES)]
-        ),
-        vol.Optional(CONF_IR_ON, default=DEFAULT_IR_ON): cv.string,
-        vol.Optional(CONF_IR_OFF, default=DEFAULT_IR_OFF): cv.string,
-    }
-)
 
+async def async_setup_entry(
+    hass: HomeAssistantType, entry: ConfigEntry, async_add_entities
+) -> None:
+    """Set up switches for UniFi Protect integration."""
+    entry_data = hass.data[DOMAIN][entry.entry_id]
+    upv_object = entry_data["upv"]
+    protect_data = entry_data["protect_data"]
 
-async def async_setup_platform(hass, config, async_add_entities, _discovery_info=None):
-    """Set up an Unifi Protect Switch."""
-    upv = hass.data[UPV_DATA]["upv"]
-    coordinator = hass.data[UPV_DATA]["coordinator"]
-    if not coordinator.data:
+    if not protect_data.data:
         return
 
-    ir_on = config.get(CONF_IR_ON)
+    ir_on = entry.data[CONF_IR_ON]
     if ir_on == "always_on":
         ir_on = "on"
 
-    ir_off = config.get(CONF_IR_OFF)
+    ir_off = entry.data[CONF_IR_OFF]
     if ir_off == "led_off":
         ir_off = "autoFilterOnly"
     elif ir_off == "always_off":
         ir_off = "off"
 
     switches = []
-    for switch_type in config.get(CONF_MONITORED_CONDITIONS):
-        for camera in coordinator.data:
+    for switch in SWITCH_TYPES:
+        for camera in protect_data.data:
+            # Only Add Switches if Camera supports it.
+            if switch == "high_fps" and not protect_data.data[camera].get(
+                "has_highfps"
+            ):
+                # High FPS is only supported on certain cameras
+                continue
+            if switch == "hdr_mode" and not protect_data.data[camera].get("has_hdr"):
+                continue
+
             switches.append(
-                UnifiProtectSwitch(coordinator, upv, camera, switch_type, ir_on, ir_off)
+                UnifiProtectSwitch(
+                    upv_object,
+                    protect_data,
+                    camera,
+                    switch,
+                    ir_on,
+                    ir_off,
+                )
             )
+            _LOGGER.debug("UNIFIPROTECT SWITCH CREATED: %s", switch)
 
-    async_add_entities(switches, True)
+    async_add_entities(switches)
 
 
-class UnifiProtectSwitch(SwitchDevice):
+class UnifiProtectSwitch(UnifiProtectEntity, SwitchDevice):
     """A Unifi Protect Switch."""
 
-    def __init__(self, coordinator, upv, camera, switch_type, ir_on, ir_off):
+    def __init__(self, upv_object, protect_data, camera_id, switch, ir_on, ir_off):
         """Initialize an Unifi Protect Switch."""
-        self.coordinator = coordinator
-        self.upv = upv
-        self._camera_id = camera
-        self._camera = self.coordinator.data[camera]
-        self._name = "{0} {1} {2}".format(
-            DOMAIN.capitalize(), SWITCH_TYPES[switch_type][0], self._camera["name"]
-        )
-        self._unique_id = self._name.lower().replace(" ", "_")
-        self._icon = "mdi:{}".format(SWITCH_TYPES.get(switch_type)[1])
+        super().__init__(upv_object, protect_data, camera_id, switch)
+        self.upv = upv_object
+        self._name = f"{SWITCH_TYPES[switch][0]} {self._camera_data['name']}"
+        self._icon = f"mdi:{SWITCH_TYPES[switch][1]}"
         self._ir_on_cmd = ir_on
         self._ir_off_cmd = ir_off
-        self._camera_type = self._camera["type"]
-        self._attr = SWITCH_TYPES.get(switch_type)[2]
-        self._switch_type = SWITCH_TYPES.get(switch_type)[2]
-        _LOGGER.debug(f"UNIFIPROTECT SWITCH CREATED: {self._name}")
-
-    @property
-    def unique_id(self):
-        """Return a unique ID."""
-        return self._unique_id
-
-    @property
-    def should_poll(self):
-        """Poll for status regularly."""
-        return False
+        self._switch_type = SWITCH_TYPES[switch][2]
 
     @property
     def name(self):
@@ -132,13 +107,28 @@ class UnifiProtectSwitch(SwitchDevice):
     @property
     def is_on(self):
         """Return true if device is on."""
-        camera = self.coordinator.data[self._camera_id]
         if self._switch_type == "record_motion":
-            enabled = True if camera["recording_mode"] == TYPE_RECORD_MOTION else False
+            enabled = (
+                True
+                if self._camera_data["recording_mode"] == TYPE_RECORD_MOTION
+                else False
+            )
         elif self._switch_type == "record_always":
-            enabled = True if camera["recording_mode"] == TYPE_RECORD_ALLWAYS else False
+            enabled = (
+                True
+                if self._camera_data["recording_mode"] == TYPE_RECORD_ALLWAYS
+                else False
+            )
+        elif self._switch_type == "ir_mode":
+            enabled = True if self._camera_data["ir_mode"] == self._ir_on_cmd else False
+        elif self._switch_type == "hdr_mode":
+            enabled = self._camera_data["hdr_mode"] is True
+        elif self._switch_type == "high_fps":
+            enabled = (
+                True if self._camera_data["video_mode"] == TYPE_HIGH_FPS_ON else False
+            )
         else:
-            enabled = True if camera["ir_mode"] == self._ir_on_cmd else False
+            enabled = True if self._camera_data["status_light"] == "True" else False
         return enabled
 
     @property
@@ -149,19 +139,10 @@ class UnifiProtectSwitch(SwitchDevice):
     @property
     def device_state_attributes(self):
         """Return the device state attributes."""
-        attrs = {}
-
-        attrs[ATTR_ATTRIBUTION] = DEFAULT_ATTRIBUTION
-        attrs[ATTR_BRAND] = DEFAULT_BRAND
-        attrs[ATTR_CAMERA_TYPE] = self._camera_type
-
-        return attrs
-
-    async def async_added_to_hass(self):
-        """When entity is added to hass."""
-        self.async_on_remove(
-            self.coordinator.async_add_listener(self.async_write_ha_state)
-        )
+        return {
+            ATTR_ATTRIBUTION: DEFAULT_ATTRIBUTION,
+            ATTR_CAMERA_TYPE: self._device_type,
+        }
 
     async def async_turn_on(self, **kwargs):
         """Turn the device on."""
@@ -171,17 +152,39 @@ class UnifiProtectSwitch(SwitchDevice):
         elif self._switch_type == "record_always":
             _LOGGER.debug("Turning on Constant Recording")
             await self.upv.set_camera_recording(self._camera_id, TYPE_RECORD_ALLWAYS)
-        else:
+        elif self._switch_type == "ir_mode":
             _LOGGER.debug("Turning on IR")
             await self.upv.set_camera_ir(self._camera_id, self._ir_on_cmd)
-        await self.coordinator.async_request_refresh()
+        elif self._switch_type == "hdr_mode":
+            _LOGGER.debug("Turning on HDR mode")
+            await self.upv.set_camera_hdr_mode(self._camera_id, True)
+        elif self._switch_type == "high_fps":
+            _LOGGER.debug("Turning on High FPS mode")
+            await self.upv.set_camera_video_mode_highfps(
+                self._camera_id, TYPE_HIGH_FPS_ON
+            )
+        else:
+            _LOGGER.debug("Changing Status Light to On")
+            await self.upv.set_camera_status_light(self._camera_id, True)
+        await self.protect_data.async_refresh(force_camera_update=True)
 
     async def async_turn_off(self, **kwargs):
         """Turn the device off."""
         if self._switch_type == "ir_mode":
             _LOGGER.debug("Turning off IR")
             await self.upv.set_camera_ir(self._camera_id, self._ir_off_cmd)
+        elif self._switch_type == "status_light":
+            _LOGGER.debug("Changing Status Light to Off")
+            await self.upv.set_camera_status_light(self._camera_id, False)
+        elif self._switch_type == "hdr_mode":
+            _LOGGER.debug("Turning off HDR mode")
+            await self.upv.set_camera_hdr_mode(self._camera_id, False)
+        elif self._switch_type == "high_fps":
+            _LOGGER.debug("Turning off High FPS mode")
+            await self.upv.set_camera_video_mode_highfps(
+                self._camera_id, TYPE_HIGH_FPS_OFF
+            )
         else:
             _LOGGER.debug("Turning off Recording")
             await self.upv.set_camera_recording(self._camera_id, TYPE_RECORD_NEVER)
-        await self.coordinator.async_request_refresh()
+        await self.protect_data.async_refresh(force_camera_update=True)
